974e183e7a9d0034149886b74b7c46e2bddec655
Javier Parra <javierparraoyola619@hotmail.com>
2017-12-01 08:53:12 +0100

    Modelos

--- principal/models.py
+++ principal/models.py
@@ -5,19 +5,53 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Receta(models.Model):
-    # Dato cadena, longitud máxima 100 y único
-    titulo = models.CharField(max_length=100, unique=True)
-    # Dato texto, con texto de ayuda
-    ingredientes = models.TextField(help_text='Redacta los ingredientes')
-    # Dato texto, con nombre: Preparación
-    prepacion = models.TextField(verbose_name='Preparación')
-    # Dato imagen, se almacenarán en la carpeta recetas, titulo: Imágen
-    imagen = models.ImageField(upload_to='recetas', verbose_name='Imágen')
-    # Dato Fecha y Hora, almacena la fecha actual
-    tiempo_registro = models.DateTimeField(auto_now=True)
-    # Enlace al modelo Usuario que Django ya tiene construido
-    usuario = models.ForeignKey(User)
+class Banco(models.Model):
+    entidadId = models.CharField(max_length=4, unique=True)
+    nombre = models.CharField()
 
     def __unicode__(self):
-        return self.titulo
+        return self.entidadId
+
+class Sucursal(models.Model):
+    sucursalId = models.CharField(max_length=4, unique=True)
+    entidad = models.ForeignKey(Banco)
+    direccion = models.CharField()
+    telefono = models.CharField()
+
+    def __unicode__(self):
+        return self.sucursalId
+
+class Usuario(models.Model):
+    nombreUsuario = models.CharField(unique=True)
+    password = models.CharField(password=True)
+    email = models.CharField(email=True)
+    nombre = models.CharField()
+    apellidos = models.CharField()
+
+    def __unicode__(self):
+        return self.nombreUsuario
+
+class Cuenta(models.Model):
+    numeroCuenta = models.CharField(max_length=20, unique=True)
+    usuarios = models.ForeignKey(Usuario)
+    saldo = models.CharField()
+
+    def __unicode__(self):
+        return self.numeroCuenta
+
+class TipoMovimiento(models.Model):
+    tipoMovId = models.CharField(unique=True)
+    descripcion = models.TextField(help_text='Transferencia, recibo, nómina, extracción en cajero')
+
+    def __unicode__(self):
+        return self.tipoMovId
+
+class Movimiento(models.Model):
+    fecha = models.DateTimeField()
+    numeroCuenta = models.ForeignKey(Cuenta.numeroCuenta)
+    descripcion = models.TextField()
+    tipoMov = models.ForeignKey(TipoMovimiento, unique=True)
+    euros = models.CharField()
+
+    def __unicode__(self):
+        return self.tipoMov
